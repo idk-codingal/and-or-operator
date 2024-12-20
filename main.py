342bdcf3b734d90bb990808c313b9576e2df6f68
idk-codingal <mokshnagda2020@gmail.com>
2024-12-19 17:09:16 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 3
+b = 24
+c = 7
+if a and b and c:
+    print("all numbers have boolean values as true")
+else:
+    print("atleast one number has boolean value as false")
+a = -6
+b = 0
+c = 96
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("none of the numbers are greater than 0")
+if b > 0 or c > 0:
+        print("either of the number is greater than 0")
+else:
+     print("none of the numbers are greater than 0")
---
+++ main1.py
@@ -0,0 +1,9 @@
+x = 2
+y = 3
+z = 2
+print(x != y)
+print(y != z)
+print(x, 'and' , y, 'are different')
+a = int(input("enter a number"))
+if x %2 != 0:
+    print("x is not even number")
